2f232f90aa12f2db6cd8b9f21415d0fd8c2c51e5
halleballe <81902969+halleballe@users.noreply.github.com>
2024-07-17 19:40:17 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -1,6 +1,3 @@
-document.getElementById('submit').addEventListener('click', on_submit);
-
-
 async function getSpotifyToken() {
     const clientId = '099a572a8852440e9532813fc7d7f62e';
     const clientSecret = '3471b83dee2c4ec98506a3ae63260845';
@@ -43,6 +40,40 @@ async function extractSongInfo(playlistUrl) {
     }
 }
 
+async function getImageAndNameFromPlaylist(playlistUrl) {
+    const playlistId = playlistUrl.split('/').pop();
+    console.log("url", playlistUrl);
+    console.log("id", playlistId);
+    try {
+        const token = await getSpotifyToken();
+        
+        // Fetch playlist data to get the name
+        const playlistResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        });
+        const playlistData = await playlistResponse.json();
+        const playlistName = playlistData.name;
+
+        // Fetch playlist images
+        const imagesResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/images`, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        });
+        const imagesData = await imagesResponse.json();
+        const coverImage = imagesData[0].url;
+
+        return { coverImage, playlistName };
+    } catch (error) {
+        console.error('Error fetching the playlist:', error);
+        return null;
+    }
+}
+
+// Example usage
+extractSongInfo('https://open.spotify.com/playlist/37i9dQZF1E378XSv3P3KTK');
 
 async function on_submit() {
     const playlistUrl = document.getElementById('playlistUrl').value;
@@ -66,3 +97,52 @@ async function on_submit() {
         document.getElementById('output').value = 'Error extracting song info';
     }
 }
+
+let default_playlists=["https://open.spotify.com/playlist/1n3nyu1YGK6EBmED7M2xbc", "https://open.spotify.com/playlist/1bmOCClj7jqYfcE5112nds", "https://open.spotify.com/playlist/0HFPa0UyfmVGoSvqhalTbB"]
+
+async function populatePlaylists() {
+    default_playlists.forEach(async (playlistUrl) => {
+        const { coverImage, playlistName } = await getImageAndNameFromPlaylist(playlistUrl);
+        const playlistItem = document.createElement('div');
+        playlistItem.classList.add('playlist-item', 'list-group-item');
+        playlistItem.onclick = showStartButton;
+        playlistItem.innerHTML = `
+        <img src="${coverImage}" alt="${playlistName}" class="img-thumbnail">
+        <span>${playlistName}</span>
+        <input type="hidden" value="${playlistUrl}">
+    `;
+        document.getElementById('defaultPlaylists').appendChild(playlistItem);
+        attachPlaylistItemListeners(); // Attach listeners to new items
+    });
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+async function getSelectedPlaylistUrl() {
+    const selectedPlaylist = document.querySelector('.playlist-item.selected');
+    if (selectedPlaylist) {
+        const playlistUrl = selectedPlaylist.querySelector('input').value;
+        try {
+            let songsInfo = await extractSongInfo(playlistUrl); // Use await here
+            songsInfo = shuffle(songsInfo);
+            // Save songs info to local storage
+            localStorage.setItem('songsInfo', JSON.stringify(songsInfo));
+            window.location.href = 'flashcards.html';
+        } catch (error) {
+            console.error('Error extracting song info:', error);
+            document.getElementById('output').value = 'Error extracting song info';
+        }
+    } else {
+        alert('Please select a playlist first.');
+    }
+}
+
+document.getElementById('start-button').addEventListener('click', getSelectedPlaylistUrl);
+
+populatePlaylists();
--- flash.js
+++ flash.js
@@ -1,79 +1,140 @@
-function getLyrics(artist, track) {
-    // Log the artist and track
-    console.log(artist, track);
-
-    // Construct the URL for fetching lyrics
-    let url = `https://api.lyrics.ovh/v1/${artist}/${track}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            let lyrics = data.lyrics;
-
-            // Clean up the lyrics formatting
-            if (lyrics.includes('Paroles')) {
-                lyrics = lyrics.split("\r\n")
-                lyrics = lyrics.slice(1).join('\n');
-            }
-
-            lyrics = lyrics.replace(/\[.*?\]/g, "").replace(/\(.*?\)\n/g, "");
-            if (lyrics.includes('\n\n\n\n')) {
-                lyrics = lyrics.replace(/\n\n/g, "\n");
-            }
-
-            // Split lyrics into verses and choruses
-            let versesAndChoruses = lyrics.split(/\n{2}/);
-
-            // Display lyrics on the webpage
-            let lyricsText = document.getElementById('lyrics');
-            lyricsText.innerHTML = lyrics;
-
-            // Reverse the order of verses and choruses
-            versesAndChoruses.reverse();
-            versesAndChoruses.forEach(verseOrChorus => {
-                let lines = verseOrChorus.split('\n');
-                if (lines.length >= 4) {
-                    let question = lines.slice(0, 2).join('<br>');
-                    let answer = lines.slice(2, 4).join('<br>');
-                    console.log('Question:', question);
-                    console.log('Answer:', answer);
-
-                    let questionElement = document.getElementById('question');
-                    questionElement.innerHTML = question;
-                    let answerElement = document.getElementById('answer');
-                    answerElement.innerHTML = '<span class="hidden-answer">Tap to show</span>';
-                    answerElement.dataset.answer = answer;
-
-                    // Add event listener to toggle answer visibility
-                    answerElement.addEventListener('click', function() {
-                        if (this.querySelector('.hidden-answer')) {
-                            this.innerHTML = this.dataset.answer;
-                        }
-                    });
-
-                    return;
-                }
-            });
-        });
-}
-
-let track = 0;
-
-document.getElementById('next-song').addEventListener('click', () => {
-    NextSong(1);
-});
-
-document.getElementById('previous-song').addEventListener('click', () => {
-    NextSong(-1);
-});
-
-function NextSong(increment) {
-    track += increment;
-    const songsInfo = JSON.parse(localStorage.getItem('songsInfo'));
+async function getLyrics() {
+    let count=100
+    while(count){
+        count--;
+    //when flashcard is updated, prepare the next card
+    track += 1;
+    let artist;
+    let song;
+    let songsInfo = JSON.parse(localStorage.getItem('songsInfo'));
     if (songsInfo && track < songsInfo.length && track >= 0) {
-        const currentSong = songsInfo[track];
-        document.getElementById('info').textContent = `${currentSong.firstArtist} - ${currentSong.songTitle}`;
-        getLyrics(currentSong.firstArtist, currentSong.songTitle);
+        let currentSong = songsInfo[track];
+        //save everything in local storage
+        localStorage.setItem('currentSong', JSON.stringify(currentSong));
+        artist = currentSong.firstArtist
+        song = currentSong.songTitle
+        
     } else {
         console.error('Track index out of bounds or songsInfo not found');
     }
+// Log the artist and track
+console.log("searching for", artist, song);
+
+
+song = song.split(" - ")[0];
+// Construct the URL for fetching lyrics
+let url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+try {
+    let response = await fetch(url);
+    let data = await response.json();
+    let lyrics = data.lyrics;
+
+    // Clean up the lyrics formatting
+    if (lyrics.includes('Paroles')) {
+        lyrics = lyrics.split("\r\n")
+        lyrics = lyrics.slice(1).join('\n');
+    }
+
+    lyrics = lyrics.replace(/\[.*?\]/g, "").replace(/\(.*?\)\n/g, "");
+    if (lyrics.includes('\n\n\n\n')) {
+        lyrics = lyrics.replace(/\n\n/g, "\n");
+    }
+
+    // Split lyrics into verses and choruses
+    let versesAndChoruses = lyrics.split(/\n{2}/);
+
+
+    for (let verseOrChorus of versesAndChoruses) {
+        let lines = verseOrChorus.split('\n');
+        if (lines.length >= 4) {
+            let question = lines.slice(0, 2).join('<br>');
+            let answer = lines.slice(2, 4).join('<br>');
+            localStorage.setItem('question', question);
+            localStorage.setItem('answer', answer);
+            console.log('Question:', question);
+            console.log('Answer:', answer);
+            //here everything stops
+            return;
+        }
+    }console.log("not found")
+    getLyrics();
+
+} catch (error) {
+    console.error('Error fetching lyrics:', error);
+    return null;
 }
+    }
+    }
+        
+        
+
+let track = 0;
+
+document.getElementById('start-button').addEventListener('click', () => {
+    swooshCard();
+    NextSong(1);
+});
+
+
+
+function NextSong(increment) {
+    try{
+        let currentSong = JSON.parse(localStorage.getItem('currentSong'));
+        let question = localStorage.getItem('question');
+        let answer = localStorage.getItem('answer');
+    
+        //update the flashcard
+        let questionElement = document.getElementById('question');
+        questionElement.innerHTML = question;
+        let answerElement = document.getElementById('answer');
+        answerElement.innerHTML = '<span class="hidden-answer">Tap to show</span>';
+        answerElement.dataset.answer = answer;
+    
+        // Add event listener to toggle answer visibility
+        answerElement.addEventListener('click', function() {
+            if (this.querySelector('.hidden-answer')) {
+                this.innerHTML = this.dataset.answer;
+            }
+        });
+    
+        document.getElementById('info').textContent = `${currentSong.firstArtist} - ${currentSong.songTitle}`;
+
+    }
+    catch(e){
+        console.error(e);
+    }
+
+    getLyrics();
+
+   
+}
+
+
+async function swooshCard() {
+    console.log("swooshing")
+    const card = document.getElementById('shadow');
+    card.classList.add('swoosh-up');
+    setTimeout(() => {
+        // Code to be executed after one second
+        card.classList.remove('swoosh-up');
+        const flashcardInnerHtml = document.getElementById('flashcard').innerHTML;
+        document.getElementById('shadow').innerHTML = flashcardInnerHtml;
+    }, 800);
+    
+}
+
+
+document.getElementById('shadow').addEventListener('click', revealAnswer);
+
+function revealAnswer() {
+    let answerElements = document.querySelectorAll('.hidden-answer');
+    answerElements.forEach(answerElement => {
+        let parent = answerElement.parentElement;
+        if (parent) {
+            parent.innerHTML = parent.dataset.answer;
+        }
+    });
+}
+
+
+NextSong();
